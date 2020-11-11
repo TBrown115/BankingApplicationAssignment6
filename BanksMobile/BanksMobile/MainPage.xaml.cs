8b2bff55944fbceda988f09649d9b224b960bb1d
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2020-11-11 21:21:21 +0200

    Create MainPage.xaml.cs

---
+++ BanksMobile/BanksMobile/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Banking;
+
+namespace BanksMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Bank fnb = new Bank("First National Bank", 4324, "Zevenwacht"); //new instance
+
+            Customer bob = new Customer("7766445424", "10 me at home", "Bob", "Sanders");
+            fnb.AddCustomer(bob);
+        }
+
+        private void DepositButton(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WithdrawButton(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HistoryButton(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
